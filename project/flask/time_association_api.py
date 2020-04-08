ef79a02c515b26c899c4b4c46a53b2ae76c799eb
Clara Mae Wells <wells.cl@husky.neu.edu>
2020-04-08 09:33:10 -0700

    re-export models, all api calls work

--- project/flask/time_association_api.py
+++ project/flask/time_association_api.py
@@ -6,51 +6,82 @@ from sklearn.externals import joblib
 app = connexion.FlaskApp(__name__, port=8080, specification_dir="swagger/")
 application = app.app
 
-# Load pre-trained model
+# Load pre-trained models
 filepath = './model/'
 
-# Categories and counts
+# Categories and counts models
 us_cat_count = joblib.load(filepath + 'us_cat_count.joblib')
 ca_cat_count = joblib.load(filepath + 'ca_cat_count.joblib')
 gb_cat_count = joblib.load(filepath + 'gb_cat_count.joblib')
 
-# Overall trending videos like/dislike engagement (over 10%)
+# Overall trending videos like/dislike engagement (over 10%) models
 us_ld_engagement = joblib.load(filepath + 'us_ld_engagement.joblib')
 ca_ld_engagement = joblib.load(filepath + 'ca_ld_engagement.joblib')
 gb_ld_engagement = joblib.load(filepath + 'gb_ld_engagement.joblib')
 
-# Overall trending videos comment engagement (over 2%)
+# Overall trending videos comment engagement (over 2%) models
 us_comm_engagement = joblib.load(filepath + 'us_comm_engagement.joblib')
 ca_comm_engagement = joblib.load(filepath + 'ca_comm_engagement.joblib')
 gb_comm_engagement = joblib.load(filepath + 'gb_comm_engagement.joblib')
 
-# Trending videos like/dislike engagement by category
+# Trending videos like/dislike engagement by category models
 us_cat_ld_engagement = joblib.load(filepath + 'us_cat_ld_engagement.joblib')
 ca_cat_ld_engagement = joblib.load(filepath + 'ca_cat_ld_engagement.joblib')
 gb_cat_ld_engagement = joblib.load(filepath + 'gb_cat_ld_engagement.joblib')
 
-# Trending videos comment engagement by category
+# Trending videos comment engagement by category models
 us_cat_comm_engagement = joblib.load(filepath + 'us_cat_comm_engagement.joblib')
 ca_cat_comm_engagement = joblib.load(filepath + 'ca_cat_comm_engagement.joblib')
 gb_cat_comm_engagement = joblib.load(filepath + 'gb_cat_comm_engagement.joblib')
 
-# Trending videos' title frequent itemsets
+# Trending videos' title frequent itemsets models
 freq_one_itemsets = joblib.load(filepath + 'itemset1.joblib')
 freq_two_itemsets = joblib.load(filepath + 'itemset2.joblib')
 freq_three_itemsets = joblib.load(filepath + 'itemset3.joblib')
 
-# Trending videos' title association rules
+# Trending videos' title association rules model
 assoc_rules = joblib.load(filepath + 'assoc_rules.joblib')
 
 # Implement functions
 def get_cat_count(country):
-  if country == us:
-    counts = us_cat_count
-  elif country == ca:
-    counts = ca_cat_count
+  if country == 'us':
+    return us_cat_count
+  elif country == 'ca':
+    return ca_cat_count
   else:
-    counts = gb_cat_count
-  return {"category counts": counts}
+    return gb_cat_count
+
+def get_ld_engagement(country):
+  if country == 'us':
+    return us_ld_engagement
+  elif country == 'ca':
+    return ca_ld_engagement
+  else:
+    return gb_ld_engagement
+
+def get_comm_engagement(country):
+  if country == 'us':
+    return us_comm_engagement
+  elif country == 'ca':
+    return ca_comm_engagement
+  else:
+    return gb_comm_engagement
+
+def get_cat_ld_engagement(country):
+  if country == 'us':
+    return us_cat_ld_engagement
+  elif country == 'ca':
+    return ca_cat_ld_engagement
+  else:
+    return gb_cat_ld_engagement
+
+def get_cat_comm_engagement(country):
+  if country == 'us':
+    return us_cat_comm_engagement
+  elif country == 'ca':
+    return ca_cat_comm_engagement
+  else:
+    return gb_cat_comm_engagement
 
 def get_freq_1_itemsets():
   return freq_one_itemsets
