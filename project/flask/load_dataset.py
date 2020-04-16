d25f698bfdd435afa0df854a8a8bc59b43ffa2f1
yejeelee <lee.yej@husky.neu.edu>
2020-04-16 02:35:05 -0700

    edited

--- project/flask/load_dataset.py
+++ project/flask/load_dataset.py
@@ -2,7 +2,6 @@
 To train this model, in your terminal:
 > python load_dataset.py
 """
-
 from sklearn.externals import joblib
 import pandas as pd
 
@@ -11,5 +10,4 @@ us_data = pd.read_csv('~/Desktop/data-miners/project/data/USvideos.csv')
 
 
 print('Exporting the trained model')
-#joblib.dump(count_vectorizer, 'model/linear_reg_cv.joblib')
 joblib.dump(us_data, 'model/us_data.joblib')
